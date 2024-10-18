2d6e1359ece65cc5bc571910de0594a07ab07b10
faunks <faunksystem@gmail.com>
2024-10-18 16:43:13 +0300

    Update Docs.js

--- PCTools/Docs.js
+++ PCTools/Docs.js
@@ -1,2 +1,7 @@
 // Lets say if you want like to do it so every time you throw a pokeball it prints to the console "sup dude"
 // you can use the merge function like this:
+
+let x = function(){
+  console.log("sup dude")
+}
+App.game.pokeballs.usePokeball = mergeFunctions(App.game.pokeballs.usePokeball,x)
